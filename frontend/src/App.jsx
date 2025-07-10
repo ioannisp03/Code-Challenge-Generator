a69d41f01942fee1457ef315c4f15f78ec55eb79
ioannisp03 <131459571+ioannisp03@users.noreply.github.com>
2025-07-09 23:58:42 -0400

    added some routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,21 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import ClerkProviderWithRoutes from "./auth/ClerkProviderWithRoutes";
+import { Layout } from "./layout/Layout.jsx";
+import { ChallengeGenerator } from "./challenge/ChallengeGenerator.jsx";
+import { HistoryPanel } from "./history/HistoryPanel.jsx";
+import { AuthenticationPage } from "./auth/AuthenticationPage";
 
 function App() {
   return (
     <ClerkProviderWithRoutes>
       <Routes>
-        <Route>
-          
+        <Route path="/sign-in/*" element={<AuthenticationPage />}></Route>
+        <Route path="/sign-up/*" element={<AuthenticationPage />}></Route>
+        <Route element={Layout}>
+          {/* These routes will render inside the layout component */}
+          <Route path="/" element={ChallengeGenerator}></Route>
+          <Route path="/history" element={HistoryPanel}></Route>
         </Route>
       </Routes>
     </ClerkProviderWithRoutes>
